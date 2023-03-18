584db23861d52ac06f63dffbb506137d6416025b
officialbishowb <officialbishowb@gmail.com>
2023-03-18 14:12:56 +0100

    Server class: constructor & method added

--- lib/localization_manager_plugin.dart
+++ lib/localization_manager_plugin.dart
@@ -2,7 +2,10 @@ library localization_manager_plugin;
 
 export 'models.dart';
 
+import 'package:freezed_annotation/freezed_annotation.dart';
+
 import 'models.dart';
+import 'dart:io';
 
 part 'src/plugin/launch_plugin.dart';
 part 'src/plugin/server.dart';
--- lib/src/plugin/server.dart
+++ lib/src/plugin/server.dart
@@ -1,3 +1,35 @@
 part of localization_manager_plugin;
 
-class Server {}
+class Server {
+  final String serverAddress;
+  WebSocket? _socket;
+
+  Server(this.serverAddress);
+
+  void connect() {
+    WebSocket.connect(serverAddress).then((socket) {
+      _socket = socket;
+      onOpen();
+      _socket!.listen((message) {
+        onMessage(message);
+      });
+    }).catchError((error) {
+      onError(error);
+    });
+  }
+
+  void onOpen() {
+    print('WebSocket connection opened!');
+  }
+
+  void onMessage(dynamic message) {
+    print('Received message from server: $message');
+  }
+
+  void onError(dynamic error) {
+    print('WebSocket error occurred: $error');
+  }
+
+
+  
+}
